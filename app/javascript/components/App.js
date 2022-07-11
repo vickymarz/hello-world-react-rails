25c7dab214c5c119a83c83c51d680a46501ffe0e
vickymarz <okorojiebube2@gmail.com>
2022-07-11 12:38:58 -0600

    Create redux store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Greeting />} />
+          <Route path="/" element={<Greeting greeting="hello" />} />
         </Routes>
       </BrowserRouter>
     );
