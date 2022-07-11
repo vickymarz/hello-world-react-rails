a501999b1256d5814d551b1b8bb23d461cb53792
vickymarz <okorojiebube2@gmail.com>
2022-07-11 11:35:21 -0600

    Get random data from database

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,4 +1,6 @@
 class v1::GreetingsController < ApplicationController
   def index
+    @greetings = Greeting.find(Greeting.pluck(:id).sample)
+    render json: { greetings: @greetings }.to_json
   end
 end
